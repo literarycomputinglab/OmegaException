c0ea861cb943aac3fa36ae57105c433a0dbda63d
simone <simone@t450s>
2015-11-20 15:39:03 +0100

    Exception first commit

---
+++ src/main/java/it/cnr/ilc/lc/omega/exception/InvalidURIException.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.cnr.ilc.lc.omega.exception;
+
+/**
+ *
+ * @author simone
+ */
+public class InvalidURIException extends Exception {
+
+    public InvalidURIException() {
+    }
+
+    public InvalidURIException(String message) {
+        super(message);
+    }
+
+    public InvalidURIException(Throwable cause) {
+        super(cause);
+    }
+
+    public InvalidURIException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidURIException(String message, Throwable cause,
+            boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}
